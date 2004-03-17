add12d589a3387ecd7ee46ee00ae632d31a67ba8
skink <skink>
2004-03-17 23:08:12 +0000

    Ajout des En-tetes GPL

--- araneus/Adresse.java
+++ araneus/Adresse.java
@@ -1,3 +1,26 @@
+/**
+ *  Copyright 2004 Fabien SCHWOB
+ *  
+ *  This file is part of Araneus.
+ *
+ *  Araneus is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Araneus is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Foobar; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+/**
+ *  Importation des packages nécessaires
+ */
 import java.net.*;
 import java.io.*;
 
@@ -30,7 +53,6 @@ public class Adresse{
         } catch (Exception e) {
             System.out.println("Erreur lors du telechargement");
         }
-        System.out.println(page);
         return this.page;
     }
     
